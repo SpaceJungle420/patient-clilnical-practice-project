937908515fd6b7bd48c9d591c903ac41e9acf4f4
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-22 13:16:33 -0300

    implement GET/SAVE patient methods

--- clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/controllers/PatientController.java
+++ clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/controllers/PatientController.java
@@ -3,6 +3,7 @@ package com.example.clinicals.clinicalsapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,15 @@ public class PatientController {
     @RequestMapping(value = "patients", method = RequestMethod.GET)
     public List<Patient> getPatients() {
         return repository.findAll();
+    }
+
+    @RequestMapping(value = "/patients/{id}", method = RequestMethod.GET)
+    public Patient getPatient(@PathVariable("id") int id) {
+        return repository.findById(id).get();
+    }
+
+    @RequestMapping(value = "/patients", method = RequestMethod.POST)
+    public Patient savePatient(Patient patient) {
+        return repository.save(patient);
     }
 }
