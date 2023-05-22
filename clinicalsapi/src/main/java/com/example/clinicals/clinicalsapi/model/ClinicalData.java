7f1ee04c579c7c27d0686dd66e533cc0a7b0e1b4
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-22 13:43:42 -0300

    add missing annotations

--- clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/ClinicalData.java
+++ clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/ClinicalData.java
@@ -4,6 +4,8 @@ import java.sql.Timestamp;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,6 +15,7 @@ import jakarta.persistence.Table;
 @Table(name = "clinicaldata")
 public class ClinicalData {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String componentName;
     private String componentValue;
--- clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/Patient.java
+++ clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/Patient.java
@@ -5,6 +5,8 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
@@ -12,6 +14,7 @@ import jakarta.persistence.OneToMany;
 public class Patient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String lastName;
     private String firstName;
