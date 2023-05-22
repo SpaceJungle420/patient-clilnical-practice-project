f167c2a8b68d06e57f461c55deb362d1f446a17e
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-22 13:38:23 -0300

    add missing import

--- clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/ClinicalData.java
+++ clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/model/ClinicalData.java
@@ -7,6 +7,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "clinicaldata")
