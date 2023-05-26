85cc9340c837ef011b769d3e42ce9ce263872d11
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-05-25 21:16:26 -0300

    add cross origon

--- clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/controllers/ClinicalDataController.java
+++ clinicalsapi/src/main/java/com/example/clinicals/clinicalsapi/controllers/ClinicalDataController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.example.clinicals.clinicalsapi.util.BMICalculator;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ClinicalDataController {
     private ClinicalDataRepository clinicalDataRepository;
     private PatientRepository patientRepository;
